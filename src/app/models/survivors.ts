4424afe1d09481697b5ea0bf658d953caaec9480
raglesgit <raglesgit@gmail.com>
2022-09-04 19:32:35 +0200

    init home

--- src/app/models/survivors.ts
+++ src/app/models/survivors.ts
@@ -1,5 +1,5 @@
 import {
-    ACTIONTYPE,
+  ACTIONTYPE,
   SKILLS
 } from "./skills";
 
@@ -11,7 +11,7 @@ export const SURVIVORS = {
       blue: SKILLS[70],
       yellow: SKILLS[2],
       orange: [skill(SKILLS[3], ACTIONTYPE[2]), SKILLS[1]],
-      red: [SKILLS[1], SKILLS[1], SKILLS[1]]
+      red: [SKILLS[1], SKILLS[70], SKILLS[1]]
     },
     xpc: 0,
     campaignSkills: [],
@@ -20,5 +20,5 @@ export const SURVIVORS = {
 };
 
 function skill(skill: any, option: any) {
-    return {name: skill.name.replace("[type d'action]", option), description: skill.description};
+  return { name: skill.name.replace("[type d'action]", option), description: skill.description };
 }
